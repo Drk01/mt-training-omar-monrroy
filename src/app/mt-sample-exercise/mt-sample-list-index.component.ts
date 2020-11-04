ad7555abb02119295d717cdb630f8286984814f2
Omar Monrroy <ogcm.kael@hotmail.com>
2020-11-04 15:46:15 -0600

    Mostrando el contenido de la tabla

--- src/app/mt-sample-exercise/mt-sample-list-index.component.ts
+++ src/app/mt-sample-exercise/mt-sample-list-index.component.ts
@@ -1,17 +1,25 @@
-import { Component } from "@angular/core";
-import { SelectedFarmService } from './selected-farm.service';
-import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { Farm } from './farm';
+import { Store } from "@ngrx/store";
+import { Component, OnInit } from "@angular/core";
+import { SelectedFarmService } from "./selected-farm.service";
+import { Observable, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { Farm } from "./farm";
 
+import { CargarFarms } from "./../store/actions/farms.action";
 @Component({
-  selector: 'mt-sample-list',
-  templateUrl: './mt-sample-list-index.component.html'
+  selector: "mt-sample-list",
+  templateUrl: "./mt-sample-list-index.component.html",
 })
-export class MtSampleListIndexComponent {
+export class MtSampleListIndexComponent implements OnInit {
+  farms: Farm[] = [];
+  loading: boolean;
+  constructor(private store: Store) {}
 
-  constructor() {
-
+  ngOnInit() {
+        this.store.dispatch(new CargarFarms());
+    this.store.select<Farm[]>("farms").subscribe((data) => {
+      this.farms = data.farms;
+      this.loading = data.loading;
+    });
   }
-
 }
