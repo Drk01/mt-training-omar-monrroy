2d510124ac1585ae7115dfa37d1ac3f9108bd122
Omar Monrroy <ogcm.kael@hotmail.com>
2020-11-04 07:32:00 -0600

    Eliminando error de importación y formateando código

--- src/app/mt-autocomplete/mt-autocomplete.component.ts
+++ src/app/mt-autocomplete/mt-autocomplete.component.ts
@@ -4,14 +4,13 @@ import { MockData } from "./mock-data";
 import {
   FieldSettingsModel,
   FilteringEventArgs,
-  ComboBox
+  ComboBox,
 } from "@syncfusion/ej2-dropdowns";
-import {} from  '@syncfusion/'
-import Fuse from "fuse.js";
+import { Fuse } from "fuse.js";
 
 @Component({
   templateUrl: "./mt-autocomplete.component.html",
-  selector: "mt-auto-complete"
+  selector: "mt-auto-complete",
 })
 export class MtAutoCompleteComponent implements OnInit {
   label = "select a value";
@@ -25,24 +24,23 @@ export class MtAutoCompleteComponent implements OnInit {
   filteredFieldProductsDataSource: DataManager;
   fieldProductsFields: FieldSettingsModel = {
     text: "productNo",
-    value: "productNo"
+    value: "productNo",
   };
 
-  @ViewChild('sports', {static:true}) private comboBox: ComboBox;
+  @ViewChild("sports", { static: true }) private comboBox: ComboBox;
   fuseSearch: Fuse<any>;
 
   ngOnInit() {
-    this.comboBox.text = 'Badminton';
-    this.comboBox.value = 'Badminton';
+    this.comboBox.text = "Badminton";
+    this.comboBox.value = "Badminton";
     this.autoCompleteDataSource = new DataManager(this.data);
     this.fieldProductsDataSource = new DataManager(this.fieldProductsData);
-    
   }
 
   onFiltering(args: FilteringEventArgs) {
     this.fuseSearch = new Fuse<any>(this.fieldProductsData, {
       keys: ["productNo", "description"],
-      threshold: 0.1
+      threshold: 0.1,
     });
     // const result = this.fuseSearch.search(args.text);
     // console.log(result);
