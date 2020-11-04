004f42db1b33e785b13c21d0eb710e9e9f4961ac
Omar Monrroy <ogcm.kael@hotmail.com>
2020-11-04 14:00:49 -0600

    Creando Farms Action

---
+++ src/app/store/actions/farms.action.ts
@@ -0,0 +1,24 @@
+import { Action } from "@ngrx/store";
+import { Farm } from "./../../mt-sample-exercise/farm";
+
+export const CARGANDO_FARMS = "[Farms] Cargando Farms";
+export const CARGANDO_FARMS_SUCCESS = "[Farms] Cargando Farms success";
+export const CARGANDO_FARMS_FAIL = "[Farms] Cargando Farms fail";
+
+export class CargarFarms implements Action {
+  readonly type = CARGANDO_FARMS;
+}
+
+export class CargarFarmsSucesss implements Action {
+  readonly type = CARGANDO_FARMS_SUCCESS;
+
+  constructor(public payload: any) {}
+}
+
+export class CargarFarmsFail implements Action {
+  readonly type = CARGANDO_FARMS_FAIL;
+
+  constructor(public payload: any) {}
+}
+
+export type farmsActions = CargarFarms | CargarFarmsSucesss | CargarFarmsFail;
