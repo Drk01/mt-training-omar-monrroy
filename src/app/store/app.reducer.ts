137fe2f60408f3922c079a4df56637b4bdd6b8ab
Omar Monrroy <ogcm.kael@hotmail.com>
2020-11-04 16:13:32 -0600

    Registrando selectedFarm en el appState

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -4,8 +4,10 @@ import * as listReducers from "./reducers/index";
 
 export interface AppState {
   farms: listReducers.FarmsState;
+  selectedFarm: listReducers.selectedFarmState;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
   farms: listReducers.farmsReducer,
+  selectedFarm: listReducers.selectedFarmReducer,
 };
