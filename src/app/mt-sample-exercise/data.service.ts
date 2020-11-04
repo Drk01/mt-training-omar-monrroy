22f4db60b345c0948e99c33cccb5d099d4525057
Omar Monrroy <ogcm.kael@hotmail.com>
2020-11-04 13:42:42 -0600

    Creando data.service/fetchFarms

--- src/app/mt-sample-exercise/data.service.ts
+++ src/app/mt-sample-exercise/data.service.ts
@@ -3,4 +3,8 @@ import { Injectable } from "@angular/core";
 @Injectable({ providedIn: "root" })
 export class DataService {
   constructor(private http: HttpClient) {}
+
+  fetchFarms() {
+    return this.http.get("./assets/farm-mock-data.json");
+  }
 }
